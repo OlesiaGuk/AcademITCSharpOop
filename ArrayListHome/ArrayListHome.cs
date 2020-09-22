075069d8baa872e180c75490d833dbff0844f7b0
OlesiaGuk <olesia.v.guk@yandex.ru>
2020-09-22 09:35:10 +0700

    [ArrayListHome]

--- ArrayListHome/ArrayListHome.cs
+++ ArrayListHome/ArrayListHome.cs
@@ -8,55 +8,80 @@ namespace ArrayListHome
     {
         static void Main(string[] args)
         {
-            using (var reader = new StreamReader("..\\..\\input.txt"))
+            var list = new List<int>();
+
+            try
             {
-                var list = new List<int>();
-                string currentLine;
-
-                while ((currentLine = reader.ReadLine()) != null)
+                using (var reader = new StreamReader("..\\..\\input.txt"))
                 {
-                    list.Add(Convert.ToInt32(currentLine));
+                    string currentLine;
+
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        list.Add(Convert.ToInt32(currentLine));
+                    }
                 }
-
-                //Console.WriteLine(list.to);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при работе в файлом: " + e);
             }
 
+            Console.Write("Исходный список: ");
+            PrintList(list);
 
+            RemoveEvenNumbers(list);
 
+            Console.Write("Список без четных чисел: ");
+            PrintList(list);
 
+            var listWithoutDuplicates = CopyListWithoutRepeatingNumbers(list);
 
-            /*
-             * ArrayList<Integer> list = new ArrayList<>();
-
-        try (Scanner scanner = new Scanner(new FileInputStream("./ArrayListHome/src/guk/1.txt"))) {
-            while (scanner.hasNext()) {
-                list.add(scanner.nextInt());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден: " + e.getMessage());
-        } catch (SecurityException e) {
-            System.out.println("Доступ к файлу запрещен: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Ошибка при работе в файлом: " + e.getMessage());
+            Console.Write("Список без повторений: ");
+            PrintList(listWithoutDuplicates);
         }
-        System.out.println("Исходный список: " + list);
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) % 2 == 0) {
-                list.remove(i);
-                i--;
-            }
-        }
-        System.out.println("Список без четных чисел: " + list);
-
-        ArrayList<Integer> list2 = new ArrayList<>();
-        for (Integer e : list) {
-            if (!list2.contains(e)) {
-                list2.add(e);
+        public static void RemoveEvenNumbers(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 == 0)
+                {
+                    list.RemoveAt(i);
+                    i--;
+                }
             }
         }
-        System.out.println("Список list без повторений: " + list2);
-             */
+
+        public static void PrintList(List<int> list)
+        {
+            Console.Write("[");
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                Console.Write(list[i] + ", ");
+            }
+
+            Console.WriteLine(list[list.Count - 1] + "]");
+        }
+
+        public static List<int> CopyListWithoutRepeatingNumbers(List<int> list)
+        {
+            List<int> newList = new List<int>();
+
+            foreach (int e in list)
+            {
+                if (!newList.Contains(e))
+                {
+                    newList.Add(e);
+                }
+            }
+
+            return newList;
         }
     }
 }
