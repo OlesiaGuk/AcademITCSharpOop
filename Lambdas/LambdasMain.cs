510cb4eed3fc4e844f7c8512d8fd7e28ab5ae7e5
OlesiaGuk <olesia.v.guk@yandex.ru>
2020-09-28 16:18:30 +0700

    [Lambdas] корректировка по замечаниям(1)

--- Lambdas/LambdasMain.cs
+++ Lambdas/LambdasMain.cs
@@ -8,15 +8,14 @@ namespace Lambdas
     {
         static void Main(string[] args)
         {
-            var persons = new List<Person>();
-            persons.AddRange(new Person[]
+            var persons = new List<Person>
             {
                 new Person("Иван", 24),
                 new Person("Иван", 17),
                 new Person("Сергей", 22),
                 new Person("Мария", 16),
                 new Person("Алексей", 30)
-            });
+            };
 
             Console.WriteLine("Исходный список:");
             persons.ForEach(Console.WriteLine);
@@ -44,12 +43,14 @@ namespace Lambdas
             Console.WriteLine($"Их средний возраст = {averageAge}");
             Console.WriteLine();
 
-            var personsMap = persons
+            var averageAgesByNames = persons
                 .GroupBy(p => p.Name)
                 .ToDictionary(p => p.Key, g => g.Select(p => p.Age).Average());
 
             Console.WriteLine("Сгруппировано по именам:");
-            personsMap.Select(i => $"{i.Key} : средний возраст = {i.Value}").ToList().ForEach(Console.WriteLine);
+            averageAgesByNames.Select(i => $"{i.Key} : средний возраст = {i.Value}")
+                .ToList()
+                .ForEach(Console.WriteLine);
             Console.WriteLine();
 
             var personsFrom20To45 = persons
@@ -59,10 +60,11 @@ namespace Lambdas
             Console.WriteLine("Люди в возрасте от 20 до 45 лет:");
             personsFrom20To45.ForEach(Console.WriteLine);
 
-            Console.WriteLine("Их имена в порядке убывания возраста: " + string.Join(", ", personsFrom20To45
-                    .OrderByDescending(p => p.Age)
-                    .Select(p => p.Name)
-                    .ToList()));
+            var personsFrom20To45NamesDescending = string.Join(", ", personsFrom20To45
+                .OrderByDescending(p => p.Age)
+                .Select(p => p.Name));
+
+            Console.WriteLine("Их имена в порядке убывания возраста: " + personsFrom20To45NamesDescending);
         }
     }
 }
